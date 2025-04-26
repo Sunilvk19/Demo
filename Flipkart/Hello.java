38d53ae09676620166ac2190262073ced0618762
SunilKumar <sunil.2701k@gmail.com>
2025-04-26 12:52:38 +0530

    adding some feature

--- Flipkart/Hello.java
+++ Flipkart/Hello.java
@@ -1,5 +1,8 @@
 class HelloMain{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Hello, World! The sum of " + a + " and " + b + " is: " + c);
     }
 }
